b1e4efe8222eb07c310903879e6f965779389904
OlegJBAc <okarakulinjb@mail.ru>
2023-04-26 09:58:29 +0300

    vuex update with ERRORS!

--- src/pages/PostPageWithStore.vue
+++ src/pages/PostPageWithStore.vue
@@ -1,51 +1,44 @@
 <template>
-  <div>
-    <h1>{{ $store.state.post.limit }}</h1>
-<!--    <h1>{{ $store.state.isAuth ? 'Пользователь авторизован' : 'Авторизуйтесь пожалуйста' }}</h1>-->
-<!--    <h1>{{ $store.getters.doubleLikes }}</h1>-->
-<!--    <div>-->
-<!--      <my-button @click="$store.commit('incrementLikes')">Лайк</my-button>-->
-<!--      <my-button @click="$store.commit('decrementLikes')">Дизлайк</my-button>-->
+    <div>
+        <h1>{{ $store.state.isAuth ? 'Пользователь авторизован' : 'Авторизуйтесь пожалуйста' }}</h1>
+        <h1>Страница с постами</h1>
+        <my-input
+            v-model="searchQuery"
+            placeholder="Поиск..."
+            v-focus
+        />
 
-<!--    </div>-->
-<!--    <h1>Страница с постами</h1>-->
-<!--    <my-input-->
-<!--        v-model="searchQuery"-->
-<!--        placeholder="Поиск..."-->
-<!--        v-focus-->
-<!--    />-->
+        <div class="app__buttons">
+            <my-button @click="showDialog">Создать пост</my-button>
+            <my-select
+                v-model="selectedSort"
+                :options="sortOptions"
+            />
+        </div>
 
-<!--    <div class="app__buttons">-->
-<!--      <my-button @click="showDialog">Создать пост</my-button>-->
-<!--      <my-select-->
-<!--          v-model="selectedSort"-->
-<!--          :options="sortOptions"-->
-<!--      />-->
-<!--    </div>-->
+        <my-button @click="fetchPosts" style="margin: 15px 0">Получить посты</my-button>
+        <my-dialog v-model:show="dialogVisible">
+            <PostForm @createPost="createPost"/>
+        </my-dialog>
 
-<!--    <my-button @click="fetchPosts" style="margin: 15px 0">Получить посты</my-button>-->
-<!--    <my-dialog v-model:show="dialogVisible">-->
-<!--      <PostForm @createPost="createPost"/>-->
-<!--    </my-dialog>-->
-
-<!--    <PostList :posts="sortedAndSearchedPosts"-->
-<!--              @remove="removePost"-->
-<!--              v-if="!isPostLoading"-->
-<!--    />-->
-<!--    <div v-else>Идёт загрузка постов...</div>-->
-<!--    <div v-intersection="loadMorePosts" class="observer"></div>-->
-    <!--    <div class="page__wrapper">-->
-    <!--      <div v-for="pageNumber in totalPages"-->
-    <!--           :key="pageNumber"-->
-    <!--           class="page"-->
-    <!--           :class="{-->
-    <!--             'current-page': page === pageNumber,-->
-    <!--           }"-->
-    <!--           @click="changePage(pageNumber)"-->
-    <!--      >{{ pageNumber }}-->
-    <!--      </div>-->
-    <!--    </div>-->
-  </div>
+        <PostList :posts="sortedAndSearchedPosts"
+                  @remove="removePost"
+                  v-if="!isPostLoading"
+        />
+        <div v-else>Идёт загрузка постов...</div>
+        <div v-intersection="loadMorePosts" class="observer"></div>
+<!--        <div class="page__wrapper">-->
+<!--            <div v-for="pageNumber in totalPages"-->
+<!--                 :key="pageNumber"-->
+<!--                 class="page"-->
+<!--                 :class="{-->
+<!--                     'current-page': page === pageNumber,-->
+<!--                   }"-->
+<!--                 @click="changePage(pageNumber)"-->
+<!--            >{{ pageNumber }}-->
+<!--            </div>-->
+<!--        </div>-->
+    </div>
 </template>
 
 <script>
@@ -55,84 +48,97 @@ import MyButton from "../components/UI/MyButton.vue";
 import MyDialog from "../components/UI/MyDialog.vue";
 import PostList from "../components/PostList.vue";
 import PostForm from "../components/PostForm.vue";
-import axios from "axios";
+import {mapState, mapActions, mapGetters, mapMutations} from "vuex";
 
 export default {
-  name: "PostsPageWithStore",
-  components: {
-    MyInput,
-    MySelect,
-    MyButton,
-    MyDialog,
-    PostList,
-    PostForm,
-  },
-  data() {
-    return {
-
-    }
-  },
-  methods: {
-    createPost(post) {
-      this.posts.push(post)
-      this.dialogVisible = false
+    name: "PostsPageWithStore",
+    components: {
+        MyInput,
+        MySelect,
+        MyButton,
+        MyDialog,
+        PostList,
+        PostForm,
     },
-    removePost(post) {
-      this.posts = this.posts.filter(postItem => postItem.id !== post.id)
+    data() {
+        return {
+            dialogVisible: false,
+        }
     },
-    showDialog() {
-      this.dialogVisible = true
+    methods: {
+        ...mapMutations({
+            setPage: 'post/setPage',
+        }),
+        ...mapActions({
+            fetchPosts: 'post/fetchPosts',
+            loadMorePosts: 'post/loadMorePosts',
+        }),
+        createPost(post) {
+            this.posts.push(post)
+            this.dialogVisible = false
+        },
+        removePost(post) {
+            this.posts = this.posts.filter(postItem => postItem.id !== post.id)
+        },
+        showDialog() {
+            this.dialogVisible = true
+        },
+        // changePage(pageNumber) {
+        //   this.page = pageNumber
+        // },
     },
-    // changePage(pageNumber) {
-    //   this.page = pageNumber
-    // },
-
-
-  },
-  computed: {
-    // sortedPosts() {
-    //   return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
-    // },
-
-  }, //Не мутирует исходный массив, потому что развернули в новый массив и его сортируем.
-  watch: {
-    // selectedSort(newValue) {
-    //   this.posts.sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
-    // },// Мутирует исходный массив.
-    // page() {
-    //   this.fetchPosts()
-    // },
-
-  },
-  mounted() {
-    // this.fetchPosts()
-  },
+    computed: {
+        ...mapState({
+            posts: state => state.post.posts,
+            isPostLoading: state => state.post.isPostLoading,
+            selectedSort: state => state.post.selectedSort,
+            searchQuery: state => state.post.searchQuery,
+            page: state => state.post.page,
+            limit: state => state.post.limit,
+            totalPages: state => state.post.totalPages,
+            sortOptions: state => state.post.sortOptions,
+        }),
+        ...mapGetters({
+            sortedAndSearchedPosts: 'post/sortedAndSearchedPosts',
+        }),
+    },
+    watch: {
+        // selectedSort(newValue) {
+        //   this.posts.sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+        // },// Мутирует исходный массив.
+        // page() {
+        //   this.fetchPosts()
+        // },
+    },
+    mounted() {
+        this.fetchPosts()
+    },
 }
 </script>
 
 <style scoped>
 .observer {
-  height: 30px;
+    height: 30px;
 }
 
 .app__buttons {
-  margin: 15px 0;
-  display: flex;
-  justify-content: space-between;
+    margin: 15px 0;
+    display: flex;
+    justify-content: space-between;
 }
 
 .page__wrapper {
-  display: flex;
-  margin-top: 15px;
+    display: flex;
+    margin-top: 15px;
 }
 
 .page {
-  border: 1px solid black;
-  padding: 10px;
-  cursor: pointer;
+    border: 1px solid black;
+    padding: 10px;
+    cursor: pointer;
 }
 
 .current-page {
-  border: 2px solid teal;
+    border: 2px solid teal;
 }
 </style>
--- src/store/postModule.js
+++ src/store/postModule.js
@@ -19,6 +19,9 @@ export const postModule = {
       }
     },
     getters: {
+        // sortedPosts() {
+        //   return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+        // }, //Не мутирует исходный массив, потому что развернули в новый массив и его сортируем.
         sortedAndSearchedPosts(state) {
             return state.posts.filter(post => post[state.selectedSort].toLowerCase().includes(state.searchQuery.toLowerCase()))
         },
@@ -82,6 +85,7 @@ export const postModule = {
             }
         },
     },
+    namespaced: true,
 
 }
 
